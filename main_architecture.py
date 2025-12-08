31ee2450870cb1272f4b140a2dffae58f8a45465
Guiguithub <guillaume2aix.dly@gmail.com>
2025-12-08 11:59:42 +0100

    Update main_architecture.py

--- main_architecture.py
+++ main_architecture.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+screen.fill("black")
 
 while running:
     # poll for events
@@ -13,11 +14,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("cyan")
-
-    # RENDER YOUR GAME HERE
+            
+    
+    for i in range(15):
+        for j in range(15):
+            pygame.draw.rect(screen,"red",[40*i+345,40*j+65,40,40],3)
+            
+    
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -25,3 +28,4 @@ while running:
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
+
