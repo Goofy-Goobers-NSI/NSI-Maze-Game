c28d48c9db7bd59e4f6f5aa5f9626742408509dd
Panther Kabanosikoder <kflodab@gmail.com>
2025-12-08 20:14:16 +0100

    This file makes the src folder a Python package so other files can import from it.
    Ce fichier indique à Python que le dossier src est un package afin de pouvoir importer ses modules

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# This file makes the src folder a Python package so other files can import from it
+
+# Ce fichier indique à Python que le dossier src est un package afin de pouvoir importer ses modules 
+
+# par exemple (from src.maze import Labyrinthe)
