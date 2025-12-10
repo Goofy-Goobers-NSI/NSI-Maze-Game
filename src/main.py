5a4afbd092061e956d55d217d54cb46086f8118f
Guillaume <guillaume2aix.dly@gmail.com>
2025-12-10 20:28:01 +0100

    Fonctions choose_start et choose_end qui permetten t de choisir un départ et une fin au labyrinthe

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
 import pygame
+import cell
+from random import *
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-screen.fill("black")
+screen.fill("white")
+cells = []
+
+def choose_start():
+    cellule_possible = cells[randint(0,len(cells))]
+    while cellule_possible[0] != 340 or cellule_possible[0] != 905 or cellule_possible[1] != 60 or cellule_possible[1] != 620: # Trouver une cellule qui est sur le bord pour avoir un départ valide.
+        cellule_possible = cells[randint(0,len(cells))]
+    return cellule_possible
+    
+def choose_end(start_cell):
+    for cellule in cells:
+        if cellule.x == 600 + 2*340 - start_cell.x and cellule.y == 600 + 2*65 - start_cell.y:
+            return cellule
+
+cellule_depart = choose_start()
+cellule_fin = choose_end(cellule_depart)
+
 
 while running:
     # poll for events
@@ -17,14 +35,20 @@ while running:
     
     for i in range(15):
         for j in range(15):
-            pygame.draw.rect(screen,"red",[40*i+345,40*j+65,40,40],3)
+            new_cell = cell.Cellule(40*i+340,40*j+60)
+            cells.append(new_cell)
+            pygame.draw.rect(screen,"black",[new_cell.x,new_cell.y,40,40],1)
             
+        # while on trouve pas une cell, on en prend une random dans cells et on check ses coordonnées
+        
     
-
+    print(cells)
+        
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
+
 
