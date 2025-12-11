84d1594dae13fcdda5f84be4856dec0640b6c5db
Guiguithub <guillaume2aix.dly@gmail.com>
2025-12-11 11:38:01 +0100

    Update maze.py

--- src/maze.py
+++ src/maze.py
@@ -4,8 +4,8 @@ from random import random
 
 
 N, E, S, W = 0, 1, 2, 3  # wall index, example: walls[N] is top wall
-DX = {E: 1, W: -1, N: 0, S: 0}  # EAST/WEST move by (x+0,y+1) or (x+0, y-1), basically move right/left
-DY = {E: 0, W: 0, N: -1, S: 1}  # NORTH/SOUTH move by (x+1, y+0) or (x-1, y+0), move up/down
+DX = {E: 1, W: -1, N: 0, S: 0}  # EAST/WEST move by (x+1,y+0) or (x-1, y+0), basically move right/left
+DY = {E: 0, W: 0, N: -1, S: 1}  # NORTH/SOUTH move by (x+0, y-1) or (x+0, y+1), move up/down
 OPPOSITE = {E: W, W: E, N: S, S: N}
 # OPPOSITE is for removing walls when carving through a maze
 # The OPPOSITE map will allow for the removal of both walls of adjacent cells
